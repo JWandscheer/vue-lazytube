17fed2e98e7f25f5c49ca83d311b03878015a8cb
Jonathan Wandscheer <jwandscheer@uni-muenster.de>
2023-08-04 16:28:30 +0200

    Added Srcsets for preview image

--- src/components/common/Preview.vue
+++ src/components/common/Preview.vue
@@ -1,5 +1,5 @@
 <script>
-import {getVimeoThumbnail, getYoutubeThumbnail} from "../../utils";
+import { getVimeoThumbnail, getYoutubeThumbnail, getYoutubeThumbnailSrcset } from "../../utils";
 
 export default {
   props: {
@@ -68,40 +68,54 @@ export default {
       } else {
         return getVimeoThumbnail(this.videoID, this.defaultThumbnailQuality)
       }
-    }
+    },
+    thumbnailSrcsets() {
+      if (this.type === 'youtube') {
+        return getYoutubeThumbnailSrcset(this.videoID)
+      } else {
+        return getVimeoThumbnail(this.videoID, 'thumbnail_large').split(',')
+      }
+    },
   }
 }
 </script>
 
 <template>
-  <a class="vlt-preview" @click="$emit('click')">
+  <div class="vlt-preview" @click="$emit('click')">
     <template v-if="!onceLoaded">
-      <template v-if="isVideoFound ">
-        <img v-if="isCustomThumbnailExist" :src="customThumbnail" :alt="'Video - ' + videoTitle"
-             @error="$event.target.src=defaultThumbnail"
-        >
-        <img
-            v-else
-            :src="defaultThumbnail" :alt="'Video - ' + videoTitle"
-        >
+      <template v-if="isVideoFound">
+
+        <picture>
+          <template v-for="[format, set] of Object.entries(thumbnailSrcsets)">
+            <source v-if="format !== 'jpg'" :srcset="set" sizes="50vw" :type="'image/' + format">
+          </template>
+          <img loading="lazy" decoding="async" :srcset="thumbnailSrcsets['jpg']"
+            :type="'image/jpg'" sizes="50vw"
+            :alt="'Video - ' + videoTitle" width="100%">
+        </picture>
+
+        <!--<img v-if="isCustomThumbnailExist" :src="customThumbnail" :alt="'Video - ' + videoTitle"
+          @error="$event.target.src = defaultThumbnail">
+        <img v-else :src="defaultThumbnail" :alt="'Video - ' + videoTitle"> -->
 
         <template v-if="showTitle">
           <span class="ly-text">{{ videoTitle }}</span>
         </template>
 
 
-        <button class="ly-button-wrapper" v-show="!clicked">
+        <button class="ly-button-wrapper" aria-label="Load and Play Video" v-show="!clicked">
           <slot name="button">
-            <svg v-if="type === 'youtube'" height="100%" version="1.1" viewBox="0 0 68 48" width="100%">
+            <svg v-if="type === 'youtube'" height="100%" version="1.1" viewBox="0 0 68 48" width="100%"
+              alt="YouTube Play Button">
               <path class="ly-large-play-button-bg"
-                    d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z"
-                    fill="#f00"></path>
+                d="M66.52,7.74c-0.78-2.93-2.49-5.41-5.42-6.19C55.79,.13,34,0,34,0S12.21,.13,6.9,1.55 C3.97,2.33,2.27,4.81,1.48,7.74C0.06,13.05,0,24,0,24s0.06,10.95,1.48,16.26c0.78,2.93,2.49,5.41,5.42,6.19 C12.21,47.87,34,48,34,48s21.79-0.13,27.1-1.55c2.93-0.78,4.64-3.26,5.42-6.19C67.94,34.95,68,24,68,24S67.94,13.05,66.52,7.74z"
+                fill="#f00"></path>
               <path d="M 45,24 27,14 27,34" fill="#fff"></path>
             </svg>
-            <svg v-else height="100%" version="1.1" viewBox="0 0 68 48" width="100%">
+            <svg v-else height="100%" version="1.1" viewBox="0 0 68 48" width="100%" alt="Play Button">
               <path class="ly-large-play-button-bg--v"
-                    d="M 63 0 C 55.79 0.13 34 0 34 0 S 12.21 0.13 0 0 C 0.06 13.05 0 24 0 24 s 0.06 10.95 0 24 C 12.21 47.87 34 48 34 48 s 21.79 -0.13 34 -0 C 67.94 34.95 68 24 68 24 S 67.94 13.05 68 0 z"
-                    fill="#00adef"></path>
+                d="M 63 0 C 55.79 0.13 34 0 34 0 S 12.21 0.13 0 0 C 0.06 13.05 0 24 0 24 s 0.06 10.95 0 24 C 12.21 47.87 34 48 34 48 s 21.79 -0.13 34 -0 C 67.94 34.95 68 24 68 24 S 67.94 13.05 68 0 z"
+                fill="#00adef"></path>
               <path d="M 45,24 27,14 27,34" fill="#fff"></path>
             </svg>
           </slot>
@@ -109,11 +123,11 @@ export default {
 
         <div v-show="clicked" class="ly-loader-wrapper">
           <slot name="loader">
-              <span class="ly-ring">
-                <span></span>
-                <span></span>
-                <span></span>
-              </span>
+            <span class="ly-ring">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
           </slot>
         </div>
 
@@ -121,14 +135,14 @@ export default {
 
       <template v-else-if="!fetchingInfo">
         <div class="ly-error-container">
-            <span class="ly-error-icon">
-              <svg fill="#fff" viewBox="0 0 48 48">
-                <path d="M0 0h48v48H0V0z" fill="none"></path>
-                <path
-                    d="M22 30h4v4h-4zm0-16h4v12h-4zm1.99-10C12.94 4 4 12.95 4 24s8.94 20 19.99 20S44 35.05 44 24 35.04 4 23.99 4zM24 40c-8.84 0-16-7.16-16-16S15.16 8 24 8s16 7.16 16 16-7.16 16-16 16z"
-                    fill-opacity="0.7"></path>
-              </svg>
-            </span>
+          <span class="ly-error-icon">
+            <svg fill="#fff" viewBox="0 0 48 48">
+              <path d="M0 0h48v48H0V0z" fill="none"></path>
+              <path
+                d="M22 30h4v4h-4zm0-16h4v12h-4zm1.99-10C12.94 4 4 12.95 4 24s8.94 20 19.99 20S44 35.05 44 24 35.04 4 23.99 4zM24 40c-8.84 0-16-7.16-16-16S15.16 8 24 8s16 7.16 16 16-7.16 16-16 16z"
+                fill-opacity="0.7"></path>
+            </svg>
+          </span>
 
           <span class="ly-error-content">
             <span class="ly-error-content__reason">
@@ -142,7 +156,7 @@ export default {
 
       </template>
     </template>
-  </a>
+  </div>
 </template>
 
 <style lang="scss">
@@ -272,21 +286,24 @@ export default {
     color: #ffffff;
   }
 
-  .ly-large-play-button-bg, .ly-large-play-button-bg--v {
+  .ly-large-play-button-bg,
+  .ly-large-play-button-bg--v {
     -webkit-transition: fill .1s cubic-bezier(0.4, 0, 1, 1), fill-opacity .1s cubic-bezier(0.4, 0, 1, 1);
     transition: fill .1s cubic-bezier(0.4, 0, 1, 1), fill-opacity .1s cubic-bezier(0.4, 0, 1, 1);
     fill: #1b1b1b;
     fill-opacity: .9
   }
 
-  .ly-large-play-button-bg:hover, &:hover .ly-large-play-button-bg {
+  .ly-large-play-button-bg:hover,
+  &:hover .ly-large-play-button-bg {
     -webkit-transition: fill .1s cubic-bezier(0, 0, 0.2, 1), fill-opacity .1s cubic-bezier(0, 0, 0.2, 1);
     transition: fill .1s cubic-bezier(0, 0, 0.2, 1), fill-opacity .1s cubic-bezier(0, 0, 0.2, 1);
     fill: #f00;
     fill-opacity: 1
   }
 
-  .ly-large-play-button-bg--v:hover, &:hover .ly-large-play-button-bg--v {
+  .ly-large-play-button-bg--v:hover,
+  &:hover .ly-large-play-button-bg--v {
     -webkit-transition: fill .1s cubic-bezier(0, 0, 0.2, 1), fill-opacity .1s cubic-bezier(0, 0, 0.2, 1);
     transition: fill .1s cubic-bezier(0, 0, 0.2, 1), fill-opacity .1s cubic-bezier(0, 0, 0.2, 1);
     fill: #00adef;
@@ -333,10 +350,10 @@ export default {
     0% {
       transform: rotate(0deg);
     }
+
     100% {
       transform: rotate(360deg);
     }
   }
 }
-
 </style>
--- src/utils.js
+++ src/utils.js
@@ -1,117 +1,190 @@
-import 'whatwg-fetch'
+import "whatwg-fetch";
 
 /** Helper method to fetch oembed data  */
-const fetchingOembed = async (src, type = 'youtube') => {
-  const url = type === 'youtube' ? `https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=${getYouTubeID(src)}&format=json` : `https://vimeo.com/api/oembed.json?url=${src}`
+const fetchingOembed = async (src, type = "youtube") => {
+	const url =
+		type === "youtube"
+			? `https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v=${getYouTubeID(
+					src
+			  )}&format=json`
+			: `https://vimeo.com/api/oembed.json?url=${src}`;
 
-  return await window.fetch(url)
-}
+	return await window.fetch(url);
+};
 
 /** Helper method to dynamically create iframe  */
-const createIframe = (videoID, getTitle, iframeClass, type = 'youtube', flag = true) => {
-  let iframeEl = null
-  if (flag) {
-    iframeEl = document.createElement('iframe')
-    if (type === 'youtube') {
-      iframeEl.setAttribute('src', `https://www.youtube.com/embed/${videoID}?enablejsapi=1&autoplay=1`)
-    } else {
-      iframeEl.setAttribute('src', `https://player.vimeo.com/video/${videoID}?autoplay=1`)
-    }
-    iframeEl.setAttribute('frameborder', '0')
-    iframeEl.setAttribute('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture')
-    iframeEl.setAttribute('allowfullscreen', '1')
-    iframeEl.setAttribute('title', `${getTitle}`)
-    iframeEl.setAttribute('class', `${iframeClass}`)
-  }
+const createIframe = (
+	videoID,
+	getTitle,
+	iframeClass,
+	type = "youtube",
+	flag = true
+) => {
+	let iframeEl = null;
+	if (flag) {
+		iframeEl = document.createElement("iframe");
+		if (type === "youtube") {
+			iframeEl.setAttribute(
+				"src",
+				`https://www.youtube.com/embed/${videoID}?enablejsapi=1&autoplay=1&rel=0`
+			);
+		} else {
+			iframeEl.setAttribute(
+				"src",
+				`https://player.vimeo.com/video/${videoID}?autoplay=1`
+			);
+		}
+		iframeEl.setAttribute("frameborder", "0");
+		iframeEl.setAttribute(
+			"allow",
+			"accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+		);
+		iframeEl.setAttribute("allowfullscreen", "1");
+		iframeEl.setAttribute("title", `${getTitle}`);
+		iframeEl.setAttribute("class", `${iframeClass}`);
+	}
 
-  return iframeEl
-}
+	return iframeEl;
+};
 
 /** Helper method to check if postMessage features is available  */
 const isPostMessageSupported = () => {
-  if (!window.postMessage) {
-    return false
-  }
-  return true
-}
+	if (!window.postMessage) {
+		return false;
+	}
+	return true;
+};
 
 /** Helper method to calculate aspect  */
 const calcAspect = (aspect) => {
-  const aspects = aspect.split(':')
+	const aspects = aspect.split(":");
 
-  return typeof aspects[1] === 'undefined' ? 56.25 : aspects[1] / aspects[0] * 100
-}
+	return typeof aspects[1] === "undefined"
+		? 56.25
+		: (aspects[1] / aspects[0]) * 100;
+};
 
 /** Helper method to get youtube video ID from url  */
 const getYouTubeID = (url) => {
-  url = url.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/u)
-  /* eslint-disable no-useless-escape */
-  return (url[2] !== undefined) ? url[2].split(/[^0-9a-z_\-]/iu)[0] : url[0]
-}
+	url = url.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/u);
+	/* eslint-disable no-useless-escape */
+	return url[2] !== undefined ? url[2].split(/[^0-9a-z_\-]/iu)[0] : url[0];
+};
 
 /** Helper method to get vimeo video ID from url  */
 const getVimeoID = (url) => {
-  return new URL(url).pathname.split('/').pop()
-}
+	return new URL(url).pathname.split("/").pop();
+};
 
 /** Helper method to get Thumbnail for youtube video */
-const getYoutubeThumbnail = (video_id, quality) => {
+const getYoutubeThumbnail = (video_id, quality, webp = false) => {
+	if (video_id) {
+		if (typeof quality === "undefined") {
+			quality = "high";
+		}
+    const webp_string = webp ? "_webp" : ""
+    const format_string = webp ? "webp" : "jpg"
+
+		let quality_key = "maxresdefault"; // Max quality
+		if (quality === "default") {
+			quality_key = "default";
+		} else if (quality === "medium") {
+			quality_key = "mqdefault";
+		} else if (quality === "high") {
+			quality_key = "hqdefault";
+		} else if (quality === "standard") {
+			quality_key = "sddefault";
+		} else if (quality === "maxres") {
+			quality_key = "maxresdefault";
+		}
+
+		return (
+			"https://img.youtube.com/vi" + webp_string + "/" + video_id + "/" + quality_key + "." + format_string
+		);
+	}
+
+	return false;
+};
+
+/** Get Youtube Thumbnail srcset */
+const getYoutubeThumbnailSrcset = (video_id) => {
   if (video_id) {
-    if (typeof quality === 'undefined') {
-      quality = 'high'
-    }
+    const qualities = [
+      {
+        key: "default",
+        width: 120,
+      },
+      {
+        key: "mqdefault",
+        width: 320,
+      },
+      {
+        key: "hqdefault",
+        width: 480,
+      },
+      {
+        key: "sddefault",
+        width: 640,
+      },
+      {
+        key: "maxresdefault",
+        width: 1280,
+      },
+    ];
 
-    let quality_key = 'maxresdefault' // Max quality
-    if (quality === 'default') {
-      quality_key = 'default'
-    } else if (quality === 'medium') {
-      quality_key = 'mqdefault'
-    } else if (quality === 'high') {
-      quality_key = 'hqdefault'
-    } else if (quality === 'standard') {
-      quality_key = 'sddefault'
-    } else if (quality === 'maxres') {
-      quality_key = 'maxresdefault'
-    }
+    const formats = ['jpg', 'webp']
 
-    return 'https://img.youtube.com/vi/' + video_id + '/' + quality_key + '.jpg'
+    // Build srcsets as array with formats as keys for jpeg and webp from qualities with getYoutubeThumbnail()
+    const srcsets = {};
+    formats.forEach((format) => {
+      srcsets[format] = [];
+      qualities.forEach((quality) => {
+        srcsets[format].push(`${getYoutubeThumbnail(video_id, quality.key, format)} ${quality.width}w`);
+      });
+      srcsets[format] = srcsets[format].join(', ');
+    });
+
+    return srcsets;
   }
 
-  return false
-}
+  return false;
+};
 
 /** Helper method to get Thumbnail for vimeo video */
 const getVimeoThumbnail = (video_id, quality) => {
-  if (video_id) {
-    if (typeof quality === 'undefined') {
-      quality = 'high'
-    }
+	if (video_id) {
+		if (typeof quality === "undefined") {
+			quality = "high";
+		}
 
-    let quality_key = '960x540'
-    if (quality === 'default') {
-      quality_key = '200x150'
-    } else if (quality === 'medium') {
-      quality_key = '295x166'
-    } else if (quality === 'high') {
-      quality_key = '640x360'
-    } else if (quality === 'standard') {
-      quality_key = '960x540'
-    } else if (quality === 'maxres') {
-      quality_key = '1280x720'
-    }
-    return 'https://i.vimeocdn.com/video/' + video_id + '_' + quality_key + '.jpg'
-  }
+		let quality_key = "960x540";
+		if (quality === "default") {
+			quality_key = "200x150";
+		} else if (quality === "medium") {
+			quality_key = "295x166";
+		} else if (quality === "high") {
+			quality_key = "640x360";
+		} else if (quality === "standard") {
+			quality_key = "960x540";
+		} else if (quality === "maxres") {
+			quality_key = "1280x720";
+		}
+		return (
+			"https://i.vimeocdn.com/video/" + video_id + "_" + quality_key + ".jpg"
+		);
+	}
 
-  return false
-}
+	return false;
+};
 
 export {
-  createIframe,
-  isPostMessageSupported,
-  calcAspect,
-  fetchingOembed,
-  getYouTubeID,
-  getYoutubeThumbnail,
-  getVimeoID,
-  getVimeoThumbnail
-}
+	createIframe,
+	isPostMessageSupported,
+	calcAspect,
+	fetchingOembed,
+	getYouTubeID,
+	getYoutubeThumbnail,
+	getVimeoID,
+	getVimeoThumbnail,
+  getYoutubeThumbnailSrcset
+};
